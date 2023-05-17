703e18c8225e1dc3cd84dabc644c1b77efd979ed
juandaantoniusapakpahan <jujupapa06@gmail.com>
2023-05-17 09:59:14 +0700

    add type parameter test

---
+++ type_parameter_test.go
@@ -0,0 +1,24 @@
+package golanggenerics
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func GenericShow[T interface{}](param T) T {
+	fmt.Println(param)
+	return param
+}
+
+func TestType(t *testing.T) {
+	var intValue int = GenericShow[int](1000)
+	assert.Equal(t, 1000, intValue)
+
+	var strValue string = GenericShow[string]("Hello World")
+	assert.Equal(t, "Hello World", strValue)
+
+	var fltValue float32 = GenericShow[float32](10.22)
+	assert.Equal(t, float32(10.22), fltValue)
+}
