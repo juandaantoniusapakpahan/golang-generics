c2fb368d0b251bf272fc6feea81582bcc7b933ae
juandaantoniusapakpahan <jujupapa06@gmail.com>
2023-05-17 11:47:25 +0700

    add set type

---
+++ type_set_test.go
@@ -0,0 +1,26 @@
+package golanggenerics
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type Number interface {
+	int | int32 | int64 | float32 | float64
+}
+
+func MinNumber[T Number](number1 T, number2 T) T {
+	if number1 < number2 {
+		return number1
+	} else if number2 < number1 {
+		return number2
+	}
+	return 0
+}
+func TestSetType(t *testing.T) {
+	assert.Equal(t, 5, MinNumber[int](10, 5))
+	assert.Equal(t, 3.1, MinNumber[float64](3.1, 5))
+	assert.Equal(t, int32(3), MinNumber[int32](3, 5))
+
+}
